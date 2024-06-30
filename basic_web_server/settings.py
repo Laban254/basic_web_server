a9ed96f546ad7dd7b9557325eaf49af1cbac9b5b
laban254 <labanrotich6544@gmail.com>
2024-07-01 00:55:01 +0300

    update allowed host

--- basic_web_server/settings.py
+++ basic_web_server/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = "django-insecure-ndy(489g^qg5@8mu19=n&wp4--d^#%_t1a5v)dhkaxtz4t2^!i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['https://basic-server-hng-590a7866e13c.herokuapp.com/', 'localhost', "127.0.0.1"]
 
 
 # Application definition
