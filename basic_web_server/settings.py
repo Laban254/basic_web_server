06429a746a08de0c1ee2029267a8a36d0baa4f7c
laban254 <labanrotich6544@gmail.com>
2024-07-01 00:46:56 +0300

    allowed host

--- basic_web_server/settings.py
+++ basic_web_server/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-ndy(489g^qg5@8mu19=n&wp4--d^#%_t1a5v)dhkaxtz4t2^!i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
